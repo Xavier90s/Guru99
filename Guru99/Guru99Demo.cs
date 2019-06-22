a93d85d6bf813a595d99c47741d86fa829cb45b7
Xavier Cuadrado Villarreal <xcuadrado@pasiona.com>
2019-06-22 16:10:31 +0200

    * .gitignore:
    * Guru99.sln:
    * Guru99.csproj:
    * Guru99Demo.cs:
    * packages.config:
    * AssemblyInfo.cs:

---
+++ Guru99/Guru99Demo.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guru99
+{
+    public class Guru99Demo
+    {
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
